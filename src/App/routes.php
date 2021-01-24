acbc44d696d31e57ad24be1cc6963c0d135d6d59
LukeMcCann <developer-lukemccann@outlook.com>
2021-01-24 19:22:09 +0000

    Add JsonResponse example route

--- src/App/routes.php
+++ src/App/routes.php
@@ -1,9 +1,17 @@
 <?php 
 
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\HttpFoundation\JsonResponse;
+
     $app->get("/", function () {
         return "Hello Silex From Route1!";
     });
 
     $app->get("/route2", function () {
         return new Symfony\Component\HttpFoundation\Response("Hello Silex From Route2");
+    });
+
+    $app->get("/jsonRoute", function () {
+        return new JsonResponse(['data' => ['hello' => 'json']]);
+        // $response->headers->set('Content-Type', 'application/json');
     });
