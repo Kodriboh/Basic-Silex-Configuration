a63f3483c26667c3aa72243a9f0a04caa2661ea2
LukeMcCann <developer-lukemccann@outlook.com>
2021-01-24 16:37:39 +0000

    Added dotenv to project

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -2,9 +2,12 @@
 
     require_once __DIR__ . '/../vendor/autoload.php';
 
+    $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+    $dotenv->load();
+
     $app = new Silex\Application();
 
-    $app['debug'] = true;
+    $app['debug'] = $_ENV['APP_ENV'] === 'dev' ? true : false;
 
     require_once __DIR__ . '/../app/routes.php';
 
