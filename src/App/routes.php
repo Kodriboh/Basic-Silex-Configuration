5c8c87dfdcf79390861976a9cbceb02b74257058
LukeMcCann <developer-lukemccann@outlook.com>
2021-01-24 16:38:58 +0000

    Add route2 test

--- src/App/routes.php
+++ src/App/routes.php
@@ -3,3 +3,7 @@
     $app->get("/", function () {
         return "Hello Silex From Routes!";
     });
+
+    $app->get("/route2", function () {
+        return "This is a test for route 2";
+    });
